18faef8b5ae51c532ff02f8374284f3dea44da8e
james <jguest777@gmail.com>
2021-01-06 20:44:15 -0500

    prod test - img optimzr

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import Image from 'next/image';
+// import Image from 'next/image';
+const imgSrc = require('./Images/logo.png?resize&size=100?webp');
 
 export default function Hero() {
   const logoSpring = useSpring({
@@ -12,12 +13,13 @@ export default function Hero() {
   return (
     <div className="hero">
       <animated.div style={logoSpring}>
-        <Image
+        {/* <Image
           src="/images/logo.png"
           alt="Guest Media Logo"
           width="1720px"
           height="538"
-        />
+        /> */}
+        <img src={imgSrc.src} alt="Guest Media Logo" />
       </animated.div>
       <h1 className="tagLine">technically creative.</h1>
     </div>
