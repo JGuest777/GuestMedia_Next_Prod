5cbe693636c43d30f3f3bf1bbfa2eb05025c45a8
james <jguest777@gmail.com>
2021-01-28 13:42:43 -0500

    testing framer motion for page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../layouts/MainLayout'
+import { motion } from 'framer-motion'
 import '../styles/style.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
 	useEffect(() => {
 		// Remove the server-side injected CSS.
 		const jssStyles = document.querySelector('#jss-server-side')
@@ -22,7 +23,21 @@ function MyApp({ Component, pageProps }) {
 				/>
 			</Head>
 			<Layout>
-				<Component {...pageProps} />
+				<motion.div
+					key={router.route}
+					initial='pageInitial'
+					animate='pageAnimate'
+					variants={{
+						pageInitial: {
+							opacity: 0
+						},
+						pageAnimate: {
+							opacity: 1
+						}
+					}}
+				>
+					<Component {...pageProps} />
+				</motion.div>
 			</Layout>
 		</>
 	)
